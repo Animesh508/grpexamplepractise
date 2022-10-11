fb86d3b035dfaaa5fadad764f93267388cc18afb
sahil bagade <sahilbagade19@gmail.com>
2022-10-11 16:36:50 +0530

    abc

---
+++ SahilDemo/tebal.js
@@ -0,0 +1,16 @@
+function printTebale(s){
+    let i = 1;
+    while(i<=10){
+        console.log(i*s);
+        2++;
+
+    }
+console.log("---------------------------------")
+
+}
+let s = 1;
+while(s<=10){
+    printTebale(s);
+    s++;
+
+}
