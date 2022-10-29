3b19ca4a687865a3598b42d5d30af90879723073
nikesh gajbhiye <nikeshgajbhiye982461@gmail.com>
2022-10-29 17:10:48 +0530

    biro

---
+++ Nikesh/Nike_array_vs_obj/array_creation.js
@@ -0,0 +1,11 @@
+let arr = new Array();
+console.log(arr.length);
+var arr1 = new Array();
+console.log(arr1.length);
+let arr3 = new Array(10);
+console.log(arr3.length);
+arr3.push(1000);
+arr3[5] = 100;
+console.log(arr3);
+let arr4 = new Array(1,2,3,4,5,6,{},true,undefined, null,100);
+console.log(arr4);
---
+++ Nikesh/symbol.js
@@ -0,0 +1,15 @@
+let obj = {
+    firstName: 'John',
+    lastName: 'Doe',
+    
+}
+console.log(obj.firstName);
+let key = 'lastName';
+console.log(obj[key]);
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+let message = {
+    title:'regarding planning',
+    heading: 'war'
+}
+let uniqueKey = Symbol('any messsage')
