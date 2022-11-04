a75c3c6b386b7266c9f54a6ad8ac942a8dbb314e
nikesh gajbhiye <nikeshgajbhiye982461@gmail.com>
2022-11-04 15:54:30 +0530

    i am god

---
+++ Nikesh/01.11.2022/03.11.2022.js
@@ -0,0 +1,32 @@
+let a = 10; let b = 20;
+let sum = b-a;
+console.log(sum);
+
+let sum1 = 2-30;
+console.log(sum1);
+
+let sum3 = '10' - '4';
+console.log(sum3);
+let sum4 = 'ten' - 5;
+console.log(sum4);
+
+let sum5 = true+true+false-null;
+console.log(sum5);
+let sum6 = true - undefined;
+console.log(sum6);
+
+let sum7 = -10 -23;
+console.log(sum7);
+let sum8 = true-true;
+console.log(sum8);
+let sum9 = 'true' -1;
+console.log(sum9);
+
+console.log(10/3);
+console.log(10*3);
+console.log(10/'3');
+console.log('10'*3);
+console.log(10/true);
+console.log(10/0);
+console.log(10/null);
+console.log(10/false);
---
+++ Nikesh/01.11.2022/test1.js
@@ -0,0 +1,35 @@
+let a = 10; let b = 20;
+let sum = a+b;
+console.log(sum);
+let sum1 = 40+50;
+console.log(sum1);
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+let s1 = 'ki';
+let s2 = 'ran';
+let sum3 = s1+s2;
+console.log(sum3);
+
+let sum4 = 'cho'+ 'du';
+console.log(sum4);
+
+let sum5 = 10 + '10';
+console.log(sum5);
+
+let sum6 = 10+20+30+'60';
+console.log(sum6);
+
+let sum7 = '60'+10+20+30+40;
+console.log(sum7);
+
+let sum8 = 1+2+''+4+5;
+console.log(sum8);
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+console.log(10 + true);
+console.log(10-false-null-true);
+console.log(true-true);
+console.log(1+undefined);
+console.log('100'+true);
+console.log(true+true+true+''+false+true+true);
+
+let sum9 = '10'-'10';
+console.log(sum9);
